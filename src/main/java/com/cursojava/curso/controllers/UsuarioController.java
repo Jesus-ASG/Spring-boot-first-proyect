5928acdfc4201b4d990a612089accda688562125
Jesus-ASG <jesus.serrano.graciano@gmail.com>
2022-09-14 18:53:09 -0500

    2-41 delete-method

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -5,6 +5,7 @@ import com.cursojava.curso.models.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -13,10 +14,11 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
+    // instanciar objeto automáticamente, no usar mucho porque se comparte en memoria
     @Autowired
     private UsuarioDao usuarioDao;
 
-    @RequestMapping(value = "usuario/{id}")
+    @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.GET)
     public Usuario getUsuario(@PathVariable Long id){
         Usuario usuario = new Usuario();
         usuario.setId(id);
@@ -27,7 +29,12 @@ public class UsuarioController {
         return usuario;
     }
 
-    @RequestMapping(value = "usuarios")
+    @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.DELETE)
+    public void eliminar(@PathVariable Long id){
+        usuarioDao.eliminar(id);
+    }
+
+    @RequestMapping(value = "api/usuarios")
     public List<Usuario> getUsuarios(){
         return usuarioDao.getUsuarios();
     }
--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -5,8 +5,7 @@ $(document).ready(function() {
 });
 
 async function cargarUsuarios(){
-
-      const request = await fetch('usuarios', {
+      const request = await fetch('api/usuarios', {
         method: 'GET',
         headers: {
           'Accept': 'application/json',
@@ -15,13 +14,15 @@ async function cargarUsuarios(){
       });
       const usuarios = await request.json();
 
+
     let listadoHTML = '';
     for (let usuario of usuarios){
+        let btnEliminar = '<a href="#" onclick="eliminarUsuario('+usuario.id+')" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a>';
         let usuarioHtml = '<tr><td>'+usuario.id
                         +'</td><td>'+usuario.nombre+' '+usuario.apellido
                         +'</td><td>'+usuario.email
                         +'</td><td>'+usuario.telefono
-                        +'</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>';
+                        +'</td><td>'+btnEliminar+'</td></tr>';
         listadoHTML += usuarioHtml;
     }
 
@@ -30,4 +31,18 @@ async function cargarUsuarios(){
     document.querySelector('#usuarios tbody').outerHTML = listadoHTML;
 
 
+}
+
+async function eliminarUsuario(id){
+    if (!confirm('¿Desea eliminar este usuario?'))
+        return;
+
+    const request = await fetch('api/usuarios/'+id, {
+        method: 'DELETE',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      });
+    location.reload()
 }
