0723c3be1ff026612a243cab4652ac394ad20baa
Jesus-ASG <jesus.serrano.graciano@gmail.com>
2022-09-14 19:43:04 -0500

    3-07 registrar

--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -18,10 +18,11 @@ async function cargarUsuarios(){
     let listadoHTML = '';
     for (let usuario of usuarios){
         let btnEliminar = '<a href="#" onclick="eliminarUsuario('+usuario.id+')" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a>';
+        let telefonoTexto = usuario.telefono ==  null ? '-' : usuario.telefono;
         let usuarioHtml = '<tr><td>'+usuario.id
                         +'</td><td>'+usuario.nombre+' '+usuario.apellido
                         +'</td><td>'+usuario.email
-                        +'</td><td>'+usuario.telefono
+                        +'</td><td>'+telefonoTexto
                         +'</td><td>'+btnEliminar+'</td></tr>';
         listadoHTML += usuarioHtml;
     }
